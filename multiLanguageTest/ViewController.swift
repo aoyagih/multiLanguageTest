b96065ecc8d43a3ecb6494fc084e3c8896865d84
aoyagih <hiroki3290@gmail.com>
2020-04-17 23:43:12 +0900

    Initial Commit

---
+++ multiLanguageTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  multiLanguageTest
+//
+//  Created by Aoyagi Hiroki on 2020/04/17.
+//  Copyright © 2020 Aoyagi Hiroki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
